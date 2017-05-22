9fd12d723baa8ccfbcb7775e63353f0d02e2d8b3
slegga <steihamm@gmail.com>
2017-05-22 05:12:52 +0000

    Fixing push-pi-ipaddr.pl

--- script/push-pi-ipaddr.pl
+++ script/push-pi-ipaddr.pl
@@ -1,24 +1,30 @@
 #!/usr/bin/env perl
 
-use Modern::Perl;
+#use Modern::Perl;
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+use Data::Dumper;
+
 use Mojo::UserAgent;
 
 my $ua = Mojo::UserAgent->new;
-my $base_dir = '/path/to/certs/';
-$ua->ca($base_dir . 'ca-cert.crt');
-$ua->cert($base_dir . 'clientcert.crt');
+#my $base_dir = '/path/to/certs/';
+#$ua->ca($base_dir . 'ca-cert.crt');
+#$ua->cert($base_dir . 'clientcert.crt');
 
-my $bio = Net::SSLeay::BIO_new_file($base_dir . 'clientcert.crt', 'r');
-my $privkey = Net::SSLeay::PEM_read_bio_PrivateKey($bio, undef, 'password');
-$ua->key($privkey);
+#my $bio = Net::SSLeay::BIO_new_file($base_dir . 'clientcert.crt', 'r');
+#my $privkey = Net::SSLeay::PEM_read_bio_PrivateKey($bio, undef, 'password');
+#$ua->key($privkey);
 
-my $tx = $ua->post('https://hjernen.glemte.no/api/pi'=>{ip=>'1.2.3.4'});
+my $tx = $ua->post('https://hjernen.glemte.no/api/pi'=> json => {ip=>'1.2.3.4'});
 
 if(my $res = $tx->success) {
   say $res->body;
   print Dumper($tx);
 } else {
   my ($err, $code) = $tx->error;
-  say $code ? "$code response: $err" : "Connection error: $err";
+  say $code ? "$code response: $err" : "Connection error:". Dumper $err;
 }
 
