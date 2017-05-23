8e0317c7898da21a696e60847efe42aac84e6476
slegga <steihamm@gmail.com>
2017-05-23 19:48:15 +0200

    Push all pi data

--- script/push-pi-ipaddr.pl
+++ script/push-pi-ipaddr.pl
@@ -7,9 +7,28 @@ use autodie;
 use feature 'say';
 use Data::Dumper;
 
+
 use Mojo::UserAgent;
 
+###
 my $ua = Mojo::UserAgent->new;
+$ua->max_redirects(5);
+my $value_hr = $ua->get('http://ip-api.com/json/')->res->json;
+
+my $uname = `uname -a`;
+my $cels;
+if ($uname=~/raspb/i) {
+     $cels=`/opt/vc/bin/vcgencmd measure_temp`;
+  ($value_hr->{temp}) = ($cels=~/temp\=([\d\.\,\w\-\+]+.\w)/);
+} elsif ($uname=~/msys/i) {
+    $cels='';
+} else {
+    $cels =`sensors`;
+  ($value_hr->{temp}) = ($cels=~/temp1\:\s+([\d\.\,\w\-\+]+)/);
+}
+$value_hr->{time} = strftime("%Y-%m-%d %H:%M:%S", localtime);
+printf j( $value_hr);
+###
 #my $base_dir = '/path/to/certs/';
 #$ua->ca($base_dir . 'ca-cert.crt');
 #$ua->cert($base_dir . 'clientcert.crt');
@@ -18,7 +37,7 @@ my $ua = Mojo::UserAgent->new;
 #my $privkey = Net::SSLeay::PEM_read_bio_PrivateKey($bio, undef, 'password');
 #$ua->key($privkey);
 
-my $tx = $ua->post('https://hjernen.glemte.no/api/set_pi_ip'=> json => {ip=>'1.2.3.4'});
+my $tx = $ua->post('https://hjernen.glemte.no/api/pi'=> json => $value_hr);
 
 if(my $res = $tx->success) {
   say $res->body;
