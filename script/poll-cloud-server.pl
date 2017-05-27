f9710682d1e5c9fcb2acc53b69b145d48f951bc4
Stein Hammer <sslegga@gmail.com>
2017-05-27 19:56:43 +0200

    fix poll server

--- script/poll-cloud-server.pl
+++ script/poll-cloud-server.pl
@@ -1,16 +1,18 @@
-#/usr/bin/env perl
+#! /usr/bin/env perl
 
 use warnings;
 use strict;
 use autodie;
-
+use feature 'say';
 use IO::Socket;
 $| = 1;
 
-my $socket = IO::Socket::INET->new(PeerAddr => $target , PeerPort => $port , Proto => 'tcp' , Timeout => 10);
+my $socket = IO::Socket::INET->new(PeerAddr => '64.137.192.243' , PeerPort => 22 , Proto => 'tcp' , Timeout => 10);
+
 #Check connection
 if( $socket )    {
-# OK
+    say time . ' ok';
 } else {
+    say time . ' unreachable';
    # https://github.com/cloudatcost/api
 }
