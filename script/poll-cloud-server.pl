0c243998ea35228eaf3d6b725b304a0ba891e3f0
Stein Hammer <sslegga@gmail.com>
2017-08-31 22:25:11 +0200

    .

--- script/poll-cloud-server.pl
+++ script/poll-cloud-server.pl
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use autodie;
 use feature 'say';
-use IO::Socket;
+use IO::Socket::PortState qw(check_ports);
 use Mojo::UserAgent;
 use YAML::Tiny;
 use Data::Dumper;
@@ -14,7 +14,8 @@ $| = 1;
 my $cfg = YAML::Tiny->read("$ENV{HOME}/etc/poll-cloud-server.yml")->[0]; #first element
 
 say Dumper $cfg;
-my $socket = IO::Socket::INET->new(PeerAddr => $cfg->{PeerAddr} , PeerPort => $cfg->{PeerPort} , Proto => $cfg->{Proto} , Timeout => $cfg->{Timeout});
+#TODO See http://www.perlmonks.org/?node_id=571066 instead
+# my $socket = IO::Socket::INET->new(PeerAddr => $cfg->{PeerAddr} , PeerPort => $cfg->{PeerPort} , Proto => $cfg->{Proto} , Timeout => $cfg->{Timeout});
 #Check connection
 if( $socket )    {
     say time . ' ok';
