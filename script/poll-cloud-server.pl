ab8bf2f0744e540d994dbb6b71a7eba7161991c1
slegga <steihamm@gmail.com>
2017-09-05 18:54:32 +0200

    Fix poll script

--- script/poll-cloud-server.pl
+++ script/poll-cloud-server.pl
@@ -4,24 +4,40 @@ use warnings;
 use strict;
 use autodie;
 use feature 'say';
-use IO::Socket::PortState qw(check_ports);
+use IO::Socket;
 use Mojo::UserAgent;
 use YAML::Tiny;
 use Data::Dumper;
+
+# http://www.perlmonks.org/?node_id=645259
+
 
 $| = 1;
 
 my $cfg = YAML::Tiny->read("$ENV{HOME}/etc/poll-cloud-server.yml")->[0]; #first element
 
 say Dumper $cfg;
-#TODO See http://www.perlmonks.org/?node_id=571066 instead
-# my $socket = IO::Socket::INET->new(PeerAddr => $cfg->{PeerAddr} , PeerPort => $cfg->{PeerPort} , Proto => $cfg->{Proto} , Timeout => $cfg->{Timeout});
+my $socket = IO::Socket::INET->new(PeerAddr => $cfg->{PeerAddr} , PeerPort => $cfg->{PeerPort} , Proto => $cfg->{Proto} , Timeout => $cfg->{Timeout});
 #Check connection
+my $ok=0;
 if( $socket )    {
-    say time . ' ok';
-} else {
-    printf "%s %s '%s' %si:%s", time, 'unreachable',$@,$cfg->{PeerAddr},$cfg->{PeerPort};
+	if( my $line = <$socket> ) {
+		if($line =~ /SSH/) {
+	    say time . ' ok';
+			$ok=1;
+		} else {
+			say time . ' ERROR: Connected but expect SSH got:'.$line;
+		}
+	} else {
+		say time . ' ERROR: Connected but no answear';
+	}
+} 
+
+else {
+    printf "%s ERROR: unreachable '%s' %si:%s", time,$@,$cfg->{PeerAddr},$cfg->{PeerPort};
    # https://github.com/cloudatcost/api
+}
+if(! $ok ) {
    my $ua  = Mojo::UserAgent->new;
   my $tx = $ua->post('https://panel.cloudatcost.com/api/v1/powerop.php' => form
  => {key=>$cfg->{key}, login=>$cfg->{username}, sid=>$cfg->{sid}, action=>'reset'});
