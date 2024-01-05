16fb8f3e308b91994baaf9f01192ae450953f628
Stein Hammer <steihamm@gmail.com>
2024-01-05 14:35:26 +0100

    Try to fix error message about sucess

--- script/poll-cloud-server.pl
+++ script/poll-cloud-server.pl
@@ -24,42 +24,57 @@ Script for polling cloud server and for figure out if it is up or not.
 
 $| = 1;
 
-my $cfg = YAML::Tiny->read("$ENV{HOME}/etc/poll-cloud-server.yml")->[0]; #first element
+my $cfg = YAML::Tiny->read("$ENV{HOME}/etc/poll-cloud-server.yml")->[0];    #first element
 
 # say Dumper $cfg;
-my $socket = IO::Socket::INET->new(PeerAddr => $cfg->{PeerAddr} , PeerPort => $cfg->{PeerPort} , Proto => $cfg->{Proto} , Timeout => $cfg->{Timeout});
+my $socket = IO::Socket::INET->new(
+    PeerAddr => $cfg->{PeerAddr},
+    PeerPort => $cfg->{PeerPort},
+    Proto    => $cfg->{Proto},
+    Timeout  => $cfg->{Timeout}
+);
+
 #Check connection
-my $ok=0;
-if( $socket )    {
-	if( my $line = <$socket> ) {
-		if($line =~ /SSH/) {
-	    say time . ' ok';
-			$ok=1;
-		} else {
-			say time . ' ERROR: Connected but expect SSH got:'.$line;
-		}
-	} else {
-		say time . ' ERROR: Connected but no answear';
-	}
+my $ok = 0;
+if ($socket) {
+    if (my $line = <$socket>) {
+        if ($line =~ /SSH/) {
+            say time . ' ok';
+            $ok = 1;
+        }
+        else {
+            say time . ' ERROR: Connected but expect SSH got:' . $line;
+        }
+    }
+    else {
+        say time . ' ERROR: Connected but no answear';
+    }
 }
 
 else {
-    printf "%s ERROR: unreachable '%s' %s:%s\n", time,$@,$cfg->{PeerAddr},$cfg->{PeerPort};
-   # https://github.com/cloudatcost/api
+    printf "%s ERROR: unreachable '%s' %s:%s\n", time, $@, $cfg->{PeerAddr}, $cfg->{PeerPort};
+
+    # https://github.com/cloudatcost/api
 }
-if(! $ok ) {
-   my $ua  = Mojo::UserAgent->new;
-  my $tx = $ua->post('https://panel.cloudatcost.com/api/v1/powerop.php' => form
- => {key=>$cfg->{key}, login=>$cfg->{username}, sid=>$cfg->{sid}, action=>'reset'});
-  if (my $res = $tx->success) {
-		printf "SUCCESS REBOOT: %s\n",$res->body;
-	}
-  else {
-    my $err = $tx->error;
-		if ($err->{code}) {
-	    printf "ERROR REBOOT: %s response: %s\n",$err->{code}, $err->{message};
-		} else {
-			printf "Connection error: %s\n",($err->{message}//'NO ERROR MESSAGE OR ANY THING.');
-		}
-  }
+if (!$ok) {
+    my $ua = Mojo::UserAgent->new;
+    my $tx = $ua->post('https://panel.cloudatcost.com/api/v1/powerop.php' => form =>
+            {key => $cfg->{key}, login => $cfg->{username}, sid => $cfg->{sid}, action => 'reset'});
+    my $res = $tx->result;
+    if ($res->is_success) {
+        printf "SUCCESS REBOOT: %s\n", $res->body;
+    }
+    else {
+        my $err = $tx->error;
+        if ($err->{code}) {
+            my $msg = sprintf "ERROR REBOOT: %s response: %s\n", $err->{code}, $err->{message};
+            say $msg;
+            say $STDERR $msg;
+        }
+        else {
+            my $msg = sprintf "Connection error: %s\n", ($err->{message} // 'NO ERROR MESSAGE OR ANY THING.');
+            say $msg;
+            say STDERR $msg;
+        }
+    }
 }
